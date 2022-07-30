cfbcfc52f15552da01990f9e64f56c1eac28d266
Mahmoud-AbdEl-khalek <m.7.mod.abd.l.khalek@gmail.com>
2022-07-30 12:40:23 +0200

    step 7

--- lib/constans/routes.dart
+++ lib/constans/routes.dart
@@ -1,3 +1,6 @@
+import 'package:test_app_2/views/verify_email_view.dart';
+
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
+const verifyEmailRoute = '/verify-email/';
